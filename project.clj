f442c7b96e1fe3dfb6908de808e377101d1cb3b4
Luo Tianj <lt@basecity.com>
2018-05-03 11:56:51 +0800

    add travis ci integration

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject fun-map "0.1.0-SNAPSHOT"
   :description "a map implementation full of fun"
+  :min-lein-version "2.7.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
