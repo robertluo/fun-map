ff229b339127c76e484e848ce4a178ff3b9e78c1
Luo Tianj <lt@basecity.com>
2018-05-07 01:10:33 +0800

    change dep scope

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :min-lein-version "2.7.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]])
