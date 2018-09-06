6252ebbbc6e32ce7f4b632301e005781af737829
Luo Tian <lt@basecity.com>
2018-09-06 14:06:11 +0800

    Update documents

--- src/robertluo/fun_map.clj
+++ src/robertluo/fun_map.clj
@@ -6,17 +6,32 @@
 (defn fun-map
   "Returns a new fun-map.
 
-  when put a function as a value in a fun-map, it will be invoked
-  with the map itself and returns the value as the value
-  when referred by the key associated, and only
-  be invoked once. "
+  A fun-map is a special map which will automatically *unwrap* a value if it's a 
+  wrapper when accessed by key. A wrapper is anything which wrapped a ordinary value
+  inside. Many clojure data structures are wrapper, such as atom, ref, future, delay,
+  agent etc. In fact, anything implements clojure.lang.IDRef interface is a wrapper.
+  
+  FuntionWrapper is another wrapper can be used in a fun-map, which wraps a function,
+  it will be called with the fun-map itself as the argument.
+  
+  Map m is the underlying storage of a fun-map, fun-map does not change its property
+  except accessing values.
+  
+  Options:
+  
+   - :trace-fn An Effectful function for globally FunctionWrapper calling trace which
+     accept key and value as its argument.
+  
+  Example:
+  
+    (fun-map {:a 35 :b (delay (println \"hello from b!\"))}"
   [m & {:keys [trace-fn]}]
   (with-meta
     (impl/delegate-map m)
     {::impl/trace trace-fn}))
 
 (defn touch
-  "forcefully evaluate all entries of a map"
+  "Forcefully evaluate all entries of a map and returns itself."
   [m]
   (doseq [[_ _] m] nil)
   m)
@@ -33,18 +48,24 @@
    (impl/fn-wrapper f trace-fn focus-fn)))
 
 (defmacro fw
-  "Define an anonymous function in place to be used in fun-map.
-  When accessed by key, it will be called with the map itself. So the arg-map
-  is a map following clojure's destructure syntax with two additional optional
-  keys:
+  "Returns a FunctionWrapper of an anonymous function defined by body.
 
-   - :focus A form that will be called to check if the function itself need
-     to be called. It must be pure functional and very quick.
-   - :trace A trace function, if the value updated, it will be called with key
-     and the vale.
+   Since a FunctionWrapper's function will be called with the map itself as the
+   argument, this macro using a map `arg-map` as its argument. It follows the 
+   same syntax of clojure's associative destructure. You may use `:keys`, `:as`,
+   `:or` inside.
+  
+   Two special keys available though:
 
-  Example:
-    (fw {:keys [a b] :as m
+    - :focus A form that will be called to check if the function itself need
+      to be called. It must be pure functional and very effecient.
+    - :trace A trace function, if the value updated, it will be called with key
+      and the function's return value.
+
+   Example:
+  
+    (fw {:keys  [a b]
+         :as    m
          :trace (fn [k v] (println k v))
          :focus (select-keys m [:a :b])}
       (+ a b))"
@@ -59,9 +80,8 @@
       ~(when focus `(fn [~arg-map] ~focus)))))
 
 (defmacro fnk
-  "Define an anonymous function intended to use in fun-map.
-  Its arguments will be take from the corresponding keys of the map, and
-  the value will be changed when its dependencies change."
+  "A shortcut for `fw` macro. Returns a simple FunctionWrapper which depends on
+  `args` key of the fun-map, it will *focus* on the keys also."
   {:style/indent 1}
   [args & body]
   `(fw {:keys  ~args
@@ -85,24 +105,27 @@
 (defn life-cycle-map
   "returns a fun-map can be shutdown orderly.
 
-   any value supports `Closeable` in this map will be considered as a
-   component, it will be closed in reversed order of its invoking.
+   Any FunctionWrapper supports `Closeable` in this map will be considered
+   as a component, its `close` method will be called in reversed order of its
+   creation when the map itself closing.
+  
    Notice only accessed components will be shutdown."
   [m]
   (let [components (atom [])
-        sys        (fun-map m
-                            :trace-fn (fn [_ v]
-                                        (when (satisfies? Haltable v)
-                                          (swap! components conj v))))
+        trace-fn (fn [_ v]
+                   (when (satisfies? Haltable v)
+                     (swap! components conj v)))
+        sys        (fun-map m :trace-fn trace-fn)
         halt-fn (fn [_]
                   (doseq [component (reverse @components)]
                     (halt! component)))]
     (vary-meta sys assoc ::impl/close-fn halt-fn)))
 
 (defn closeable
-  "returns a wrapped plain value, which implements IDref and Closeable,
+  "Returns a wrapped plain value, which implements IDref and Closeable,
    the close-fn is an effectual function with no argument.
-   when used inside a life cycle map, its close-fn when get called when
+  
+   When used inside a life cycle map, its close-fn when get called when
    closing the map."
   [r close-fn]
   (impl/->CloseableValue r close-fn))
