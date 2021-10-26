317a57caa254fb636df224385f2472a4e0e5c426
Luo Tian <lt@basecity.com>
2021-10-26 11:00:04 +0800

    implement transient (#36)

--- test/robertluo/fun_map/core_test.clj
+++ test/robertluo/fun_map/core_test.clj
@@ -10,3 +10,12 @@
          (sut/destruct-map '{:keys  [a b :d/f] c :c :d/keys [e] :as this
                              :or {a 3}
                              :focus [a b]}))))
+
+(deftest transient-test
+  (letfn [(wrap-m [m f] (-> m sut/delegate-map transient f persistent!))]
+    (is (= {} (wrap-m {} identity)))
+    (is (= {:a 1} (wrap-m {} #(assoc! % :a 1))))
+    (is (= {:a 1} (wrap-m {} #(conj! % [:a 1]))))
+    (is (= 2 (-> (sut/delegate-map {:a (delay 2)}) transient (.valAt :a))))
+    (is (= 2 (-> (sut/delegate-map {:a 1 :b 2}) count)))
+    (is (= {:a 1} (wrap-m {:a 1 :b 2} #(dissoc! % :b))))))
